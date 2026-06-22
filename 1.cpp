18845c3c457f6c9500297c436774e428f13f07e1
Nishita Upreti <nisihita7545@gmail.com>
2026-06-22 14:16:43 +0530

    leetcode:-724

--- 1.cpp
+++ 1.cpp
@@ -7,3 +7,36 @@ int main(){
         cout << arr[i] << " ";
     }
 }
+
+// leetcode 724
+// pivot index:- 
+int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int p1 = 0,p2=0, left[n], right[n], j=n-1;
+        for(int i=0; i<n; i++){
+            left[i] = nums[i]+p1;
+            p1 = left[i];
+
+            right[j] = p2+nums[j];
+            p2 = right[j];
+            j--;
+        }
+        for(int i=0; i<n; i++){
+            int l,r;
+            if(i==0){
+                l = 0;
+            }else{
+                l = left[i-1];
+            }
+            if(i==n-1){
+                r=0;
+            }else{
+                r = right[i+1];
+            }
+            if(l==r){
+                return i;
+            }  
+             
+        }
+        return -1;
+    }
